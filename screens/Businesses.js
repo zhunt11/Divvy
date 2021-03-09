d99818fd24dc53917d04ccfd8dad0f2e4e777e45
Zachary Hunt <zach.hunt@connectfss.com>
2021-03-08 17:06:18 -0700

    List of businesses now rendering.

--- screens/Businesses.js
+++ screens/Businesses.js
@@ -1,9 +1,50 @@
 import * as React from 'react';
-import { FlatList, Text } from 'react-native';
+import { View, FlatList, Text, SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import CompanyData from '../data.json'
 
 export default class Businesses extends React.Component {
   render() {
-    return <Text>Foo bar shibby</Text>
-    // return <FlatList />
+    return (
+      <SafeAreaView style={styles.container}>
+      <FlatList
+      data={CompanyData.sort((a, b) => a.name.localeCompare(b.name))}
+      renderItem={renderItem}
+      ItemSeparatorComponent={ItemSeparatorView}
+      keyExtractor={item => item.id}
+      />
+      </SafeAreaView>
+    );
   }
 }
+
+const screenPadding = 16;
+
+const renderItem = ({ item }) => (
+  <Item item={item} />
+);
+
+const Item = ({ item }) => (
+  <View style={styles.item}>
+    <Text style={styles.name} onPress={() => navigation.navigate("CompanyDetail", { item: item })}>{item.name}</Text>
+  </View>
+);
+
+const ItemSeparatorView = () => {
+  return (
+    <View style={{ height: 0.5, width: '100%', backgroundColor: '#C8C8C8' }}/>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#ffffff',
+    padding: 12
+  },
+  name: {
+    fontSize: 24,
+  },
+});
